f571730c9aae2278124f6ef9b80c46e5c69f1c18
Spasko <patsokatsarski1@gmail.com>
2023-08-10 22:43:24 +0300

    Adding functional dropdown button for Add using JavaScript

--- AirsoftWebStore/AirsoftWebStore.Web/wwwroot/js/site.js
+++ AirsoftWebStore/AirsoftWebStore.Web/wwwroot/js/site.js
@@ -1,1 +1,15 @@
-﻿
+﻿document.getElementById('dropdownMenuButton').addEventListener('click', showItems)
+
+function showItems() {
+    document.getElementById('dropdownItems').classList.toggle('show')
+}
+
+document.body.addEventListener('click', function (event) {
+    const dropdownItems = document.getElementById('dropdownItems');
+    const dropdownButton = document.getElementById('dropdownMenuButton');
+
+    // Check if the clicked element is not inside the dropdown
+    if (!dropdownItems.contains(event.target) && event.target !== dropdownButton) {
+        dropdownItems.classList.remove('show');
+    }
+})
