f9a3f0aeefa2314985412c3e6facebaabf1908a4
Spasko <patsokatsarski1@gmail.com>
2023-08-11 18:31:22 +0300

    Adding unit tests for AdminService

---
+++ AirsoftWebStore/AirsoftWebStore.Services.Tests/Admin/AdminDatabaseSeeder.cs
@@ -0,0 +1,52 @@
+﻿namespace AirsoftWebStore.Services.Tests.Admin
+{
+    using AirsoftWebStore.Data;
+    using AirsoftWebStore.Data.Models;
+
+    public static class AdminDatabaseSeeder
+    {
+        public static ApplicationUser UserWithRequest;
+        public static ApplicationUser UserWithoutRequest;
+
+        public static void SeedDatabaseForAdmin(AirsoftStoreDbContext dbContext)
+        {
+            UserWithRequest = new ApplicationUser()
+            {
+                UserName = "test@test.com",
+                NormalizedUserName = "TEST@TEST.COM",
+                Email = "test@test.com",
+                NormalizedEmail = "TEST@TEST.COM",
+                EmailConfirmed = false,
+                PasswordHash = "testestestestestes1231231232",
+                SecurityStamp = "2RC726483DB146C7AB86961EBD8FA68B",
+                PhoneNumberConfirmed = false,
+                TwoFactorEnabled = false,
+                LockoutEnabled = true,
+                AccessFailedCount = 0,
+                FirstName = "Test",
+                LastName = "Testov",
+                HasGunsmithRequest = true
+            };
+
+            UserWithoutRequest = new ApplicationUser()
+            {
+                UserName = "test2@test.com",
+                NormalizedUserName = "TEST2@TEST.COM",
+                Email = "test2@test.com",
+                NormalizedEmail = "TEST2@TEST.COM",
+                EmailConfirmed = false,
+                PasswordHash = "testestestestestes12312312322",
+                SecurityStamp = "2RC726483DB146C7AB86961EBD8FA68G",
+                PhoneNumberConfirmed = false,
+                TwoFactorEnabled = false,
+                LockoutEnabled = true,
+                AccessFailedCount = 0,
+                FirstName = "Test2",
+                LastName = "Testov2"
+            };
+
+            dbContext.Users.AddRange(UserWithRequest, UserWithoutRequest);
+            dbContext.SaveChanges();
+        }
+    }
+}
---
+++ AirsoftWebStore/AirsoftWebStore.Services.Tests/Admin/AdminServiceTests.cs
@@ -0,0 +1,50 @@
+﻿namespace AirsoftWebStore.Services.Tests.Admin
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using AirsoftWebStore.Data;
+    using AirsoftWebStore.Services.Contracts;
+    using AirsoftWebStore.Web.ViewModels.Admin;
+    using static AdminDatabaseSeeder;
+
+    public class AdminServiceTests
+    {
+        private AirsoftStoreDbContext dbContext;
+        private DbContextOptions<AirsoftStoreDbContext> dbOptions;
+
+        private IAdminService adminService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            dbOptions = new DbContextOptionsBuilder<AirsoftStoreDbContext>()
+                .UseInMemoryDatabase("AirsoftStoreInMemory" + Guid.NewGuid().ToString())
+                .Options;
+
+            dbContext = new AirsoftStoreDbContext(dbOptions);
+            SeedDatabaseForAdmin(dbContext);
+
+            adminService = new AdminService(dbContext);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            dbContext.Database.EnsureDeleted();
+            dbContext.Database.EnsureCreated();
+            SeedDatabaseForAdmin(dbContext);
+        }
+
+        [Test]
+        public async Task GetAllReqeustsShouldReturnCorrectCount()
+        {
+            const int expectedRequestsCount = 1;
+
+            ICollection<AllRequestsViewModel> models = (ICollection<AllRequestsViewModel>)await this.adminService.GetAllReqeustsAsync();
+
+            int actualRequestsCount = models.Count();
+
+            Assert.AreEqual(actualRequestsCount, expectedRequestsCount);
+        }
+    }
+}
