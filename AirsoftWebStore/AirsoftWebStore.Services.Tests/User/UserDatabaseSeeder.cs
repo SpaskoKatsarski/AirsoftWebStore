8735442ce17cf01d82fdb76ea20160d38b4ba177
Spasko <patsokatsarski1@gmail.com>
2023-08-11 18:17:03 +0300

    Adding unit tests for UserService

---
+++ AirsoftWebStore/AirsoftWebStore.Services.Tests/User/UserDatabaseSeeder.cs
@@ -0,0 +1,59 @@
+﻿namespace AirsoftWebStore.Services.Tests.User
+{
+    using AirsoftWebStore.Data;
+    using AirsoftWebStore.Data.Models;
+
+    public static class UserDatabaseSeeder
+    {
+        public static ApplicationUser NormalUser;
+        public static ApplicationUser GunsmithUser;
+
+        public static void SeedDatabaseForUser(AirsoftStoreDbContext dbContext)
+        {
+            NormalUser = new ApplicationUser()
+            {
+                UserName = "test@test.com",
+                NormalizedUserName = "TEST@TEST.COM",
+                Email = "test@test.com",
+                NormalizedEmail = "TEST@TEST.COM",
+                EmailConfirmed = false,
+                PasswordHash = "testestestestestes1231231232",
+                SecurityStamp = "2RC726483DB146C7AB86961EBD8FA68B",
+                PhoneNumberConfirmed = false,
+                TwoFactorEnabled = false,
+                LockoutEnabled = true,
+                AccessFailedCount = 0,
+                FirstName = "Test",
+                LastName = "Testov",
+                Money = 0
+            };
+
+            GunsmithUser = new ApplicationUser()
+            {
+                UserName = "test2@test.com",
+                NormalizedUserName = "TEST2@TEST.COM",
+                Email = "test2@test.com",
+                NormalizedEmail = "TEST2@TEST.COM",
+                EmailConfirmed = false,
+                PasswordHash = "testestestestestes12312312322",
+                SecurityStamp = "2RC726483DB146C7AB86961EBD8FA68G",
+                PhoneNumberConfirmed = false,
+                TwoFactorEnabled = false,
+                LockoutEnabled = true,
+                AccessFailedCount = 0,
+                FirstName = "Test2",
+                LastName = "Testov2",
+                Money = 0
+            };
+
+            Gunsmith gunsmith = new Gunsmith()
+            {
+                User = GunsmithUser
+            };
+
+            dbContext.Users.AddRange(NormalUser, GunsmithUser);
+            dbContext.Gunsmiths.Add(gunsmith);
+            dbContext.SaveChanges();
+        }
+    }
+}
---
+++ AirsoftWebStore/AirsoftWebStore.Services.Tests/User/UserServiceTests.cs
@@ -0,0 +1,71 @@
+﻿namespace AirsoftWebStore.Services.Tests.User
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using AirsoftWebStore.Data;
+    using AirsoftWebStore.Services.Contracts;
+    using static UserDatabaseSeeder;
+    using AirsoftWebStore.Web.ViewModels.User;
+
+    public class UserServiceTests
+    {
+        private AirsoftStoreDbContext dbContext;
+        private DbContextOptions<AirsoftStoreDbContext> dbOptions;
+
+        private IUserService userService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            dbOptions = new DbContextOptionsBuilder<AirsoftStoreDbContext>()
+                .UseInMemoryDatabase("AirsoftStoreInMemory" + Guid.NewGuid().ToString())
+                .Options;
+
+            dbContext = new AirsoftStoreDbContext(dbOptions);
+            SeedDatabaseForUser(dbContext);
+
+            userService = new UserService(dbContext);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            dbContext.Database.EnsureDeleted();
+            dbContext.Database.EnsureCreated();
+            SeedDatabaseForUser(dbContext);
+        }
+
+        [Test]
+        public async Task AllShouldReturnCorrectCount()
+        {
+            const int expectedCount = 2;
+
+            ICollection<UserViewModel> models = (ICollection<UserViewModel>)await this.userService.AllAsync();
+
+            int count = models.Count();
+
+            Assert.AreEqual(count, expectedCount);
+        }
+
+        [Test]
+        public async Task AllShouldDifferentiateGunsmiths()
+        {
+            const string gunsmithEmail = "test2@test.com";
+
+            IEnumerable<UserViewModel> models = await this.userService.AllAsync();
+            UserViewModel gunsmithModel = models.First(m => m.Email == gunsmithEmail);
+
+            Assert.IsTrue(gunsmithModel.IsGunsmith);
+        }
+
+        [Test]
+        public async Task GetFullNameByEmailShouldReturnFullName()
+        {
+            string expectedFullName = $"{NormalUser.FirstName} {NormalUser.LastName}";
+
+            string actualName = await this.userService.GetFullNameByEmailAsync(NormalUser.Email);
+
+            Assert.AreEqual(actualName, expectedFullName);
+        }
+    }
+}
